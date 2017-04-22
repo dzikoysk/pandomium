53ec3b36443d9e3c5b69c10be2d25d83c2840c41
dzikoysk <dzikoysk@dzikoysk.net>
2017-04-22 17:11:13 +0200

    Fix version

--- pandomium/src/main/java/org/panda_lang/pandomium/PandomiumConstants.java
+++ pandomium/src/main/java/org/panda_lang/pandomium/PandomiumConstants.java
@@ -2,7 +2,7 @@ package org.panda_lang.pandomium;
 
 public class PandomiumConstants {
 
-    public static final String PANDOMIUM_VERSION = "indev-0.1.0";
+    public static final String PANDOMIUM_VERSION = "1.0.0";
 
     public static final String PANDOMIUM_NATIVES_REPOSITORY = "https://pandomium.panda-lang.org/download/native/" + PANDOMIUM_VERSION + "/";
 
