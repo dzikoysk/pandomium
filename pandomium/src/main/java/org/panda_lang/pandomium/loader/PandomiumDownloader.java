8b485c25928a3ee7bf1fd092c9ec400ede5d6b24
dzikoysk <dzikoysk@dzikoysk.net>
2017-07-06 14:44:28 +0200

    And another one gone and another one gone

--- pandomium/src/main/java/org/panda_lang/pandomium/loader/PandomiumDownloader.java
+++ pandomium/src/main/java/org/panda_lang/pandomium/loader/PandomiumDownloader.java
@@ -1,8 +1,8 @@
 package org.panda_lang.pandomium.loader;
 
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class PandomiumDownloader {
 
@@ -13,12 +13,12 @@ public class PandomiumDownloader {
     }
 
     public InputStream download(String remotePath) {
-        System.out.println("Download: " + remotePath);
+        System.out.println("Downloading " + remotePath);
 
         try {
             URL url = new URL(remotePath);
-            URLConnection connection = url.openConnection();
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             connection.connect();
 
             return connection.getInputStream();
