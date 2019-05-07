7b445cb9ba209b774f6273e72095ca77a32a3883
dzikoysk <dzikoysk@dzikoysk.net>
2019-05-07 14:35:40 +0200

    Support ibm-jdk-8 and adoptopenjdk-8-openj9 and release 67.0.6 (Fix #39)

--- pandomium/src/main/java/org/panda_lang/pandomium/util/SystemUtils.java
+++ pandomium/src/main/java/org/panda_lang/pandomium/util/SystemUtils.java
@@ -1,51 +1,21 @@
 package org.panda_lang.pandomium.util;
 
 import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.Map;
 
 public class SystemUtils {
 
     public static void injectLibraryPath(String libraryPath) throws Exception {
         System.setProperty("java.library.path", libraryPath);
 
-        Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
-        sysPathsField.setAccessible(true);
-        sysPathsField.set(null, null);
-    }
-
-    @SuppressWarnings("unchecked")
-    public static void setEnv(Map<String, String> newEnv) {
         try {
-            Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
-            Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
-            theEnvironmentField.setAccessible(true);
-            Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-            env.putAll(newEnv);
-            Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
-            theCaseInsensitiveEnvironmentField.setAccessible(true);
-            Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
-            cienv.putAll(newEnv);
-        }
-        catch (NoSuchFieldException e) {
-            try {
-                Class[] classes = Collections.class.getDeclaredClasses();
-                Map<String, String> env = System.getenv();
-                for(Class cl : classes) {
-                    if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-                        Field field = cl.getDeclaredField("m");
-                        field.setAccessible(true);
-                        Object obj = field.get(env);
-                        Map<String, String> map = (Map<String, String>) obj;
-                        map.clear();
-                        map.putAll(newEnv);
-                    }
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
-        } catch (Exception e1) {
-            e1.printStackTrace();
+            Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+            sysPathsField.setAccessible(true);
+            sysPathsField.set(null, null);
+        } catch (final NoSuchFieldException e) {
+            /*
+             * ignore, happens on ibm-jdk-8 and adoptopenjdk-8-openj9 but has no influence since there is no cached
+             * field that needs a reset
+             */
         }
     }
 
