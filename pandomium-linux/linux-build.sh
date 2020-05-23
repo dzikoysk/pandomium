ab6c20940b5d1e392d0425c8e0535b74bf83aa35
dzikoysk <dzikoysk@dzikoysk.net>
2020-05-23 22:11:26 +0200

    GH-62 Improve linux build

--- pandomium-linux/linux-build.sh
+++ pandomium-linux/linux-build.sh
@@ -7,10 +7,10 @@
 #   Python 2.7
 #   Oracle JDK 8 with JAVA_HOME
 #   libnss3, libx11-xcb-dev, libxss1
-#   libasound2, libxtst6, unzip
+#   libasound2, libxtst6, unzip, libgbm1, maven
 #
 
-mkdir JCEF67 && cd JCEF67
+mkdir JCEF && cd JCEF
 git clone https://bitbucket.org/chromiumembedded/java-cef.git src && cd src
 
 # Modify sources
@@ -34,6 +34,8 @@ sudo apt-get install libxss1
 sudo apt-get install libasound2
 sudo apt-get install libxtst6
 sudo apt-get install unzip
+sudo apt-get install libgbm1
+sudo apt-get install maven
 
 # Build sources
 make -j4
@@ -43,7 +45,8 @@ cd ../tools/
 ./compile.sh linux64
 
 # Test
-./run.bat linux64 Release detailed
+export LD_LIBRARY_PATH="${JAVA_HOME}/jre/lib/amd64/"
+./run.sh linux64 Release detailed
 
 # Create binary distrib
 ./make_distrib.sh linux64
@@ -59,12 +62,13 @@ mkdir jcef-linux64
 (cd jcef-linux64; unzip -uo ../jcef.jar)
 (cd jcef-linux64; unzip -uo ../jogl-all.jar)
 (cd jcef-linux64; unzip -uo ../jogl-all-natives-linux-amd64.jar)
-jar -cvf jcef-linux64.jar -C jcef-linux64 .
 
 # Move output
 cd ../../../../
-cp src/binary_distrib/linux64/bin/jcef-linux64.jar linux64/jcef-linux64.jar
+# cp src/binary_distrib/linux64/bin/jcef-linux64.jar linux64/jcef-linux64.jar
+cp -r src/binary_distrib/linux64/bin/jcef-linux64/ linux64/jcef-linux64
 cp -r src/binary_distrib/linux64/bin/lib/linux64 linux64/natives
+
 mkdir -p linux64/src/org && cp -r src/java/org linux64/src/org
 
 # Remove symbols libcef.so (500MB -> 100MB)
@@ -76,3 +80,13 @@ strip linux64/natives/libcef.so
 
 # Pack natives
 cd linux64/natives/ && tar -cf - . | xz -9e -c - > ../../linux64/linux64-natives.tar.xz && cd -
+
+# Create fat jar
+jar -cvf pandomium-natives-linux64-81.2.jar -C jcef-linux64 .
+
+mvn install:install-file\
+    -Dpackaging=jar\
+    -Dfile=pandomium-linux/jcef-linux64.jar\
+    -DgroupId=org.panda-lang.pandomium-natives\
+    -DartifactId=pandomium-natives-linux64\
+    -Dversion=81.2
