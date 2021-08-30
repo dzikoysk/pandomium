365e56c815edff8949a245f913316d49119b54e0
Osiris Team <osiris_support@pm.me>
2021-08-30 18:53:40 +0200

    Updated Pandomium-Builder.jar and removed unnecessary files

--- pandomium-linux/maven-install.sh
+++
@@ -1,6 +0,0 @@
-install:install-file
-    -Dpackaging=jar
-    -Dfile=pandomium-linux/jcef-linux64.jar
-    -DgroupId=org.panda-lang.pandomium-natives
-    -DartifactId=pandomium-natives-linux64
-    -Dversion=67.0
--- pandomium-macos/maven-install.sh
+++
@@ -1,6 +0,0 @@
-install:install-file
-    -Dpackaging=jar
-    -Dfile=pandomium-macos/jcef-macos.jar
-    -DgroupId=org.panda-lang.pandomium-natives
-    -DartifactId=pandomium-natives-macos
-    -Dversion=67.0
